5ebcddac01fe94b5a449d3f038af5025be6ab6a2
igor <borispopygai@yandex.ru>
2023-07-05 05:35:01 +0700

    made common classes placeholders; fixed stylelint script; fixed stylelint errors;

--- config/stylelint/.stylelintrc.js
+++ config/stylelint/.stylelintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: "stylelint-config-standard-scss",
   rules: {
     "selector-class-pattern": /[a-z]+([A-Z][a-z]*)*/,
+    "keyframes-name-pattern": /[a-z]+([A-Z][a-z]*)*/,
+    "scss/percent-placeholder-pattern": /[a-z]+([A-Z][a-z]*)*/,
     "custom-property-empty-line-before": null,
   }
 }
