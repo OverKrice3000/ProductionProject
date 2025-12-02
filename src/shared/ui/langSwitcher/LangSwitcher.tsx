b9db88f5176b584b2cd0eefe2576af77b564baa1
igor <i.klimyuk@softaria.com>
2025-12-02 07:31:10 +0700

    lesson 28 - lang switcher short label

--- src/shared/ui/langSwitcher/LangSwitcher.tsx
+++ src/shared/ui/langSwitcher/LangSwitcher.tsx
@@ -7,17 +7,19 @@ export type LangType = "ru" | "en";
 
 interface LangSwitcherProps {
   className?: string;
+  short?: boolean;
 }
 
-export const LangSwitcher = (props: LangSwitcherProps) => {
-  const { className } = props;
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
   const { t, i18n } = useTranslation();
+
   const toggleLang = useCallback(() => {
     i18n.changeLanguage(i18n.language === `ru` ? `en` : `ru`);
   }, [i18n]);
+
   return (
       <AppButton theme={AppButtonTheme.CLEAR} className={classNames(``, {}, [className])} onClick={toggleLang}>
-        {t(`ToggleLang`)}
+        {t(short ? `Language` : `ShortLanguage`)}
       </AppButton>
   );
 };
