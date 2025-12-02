70682393bd60eb095d6cc41fb8184a260bc24b0b
igor <i.klimyuk@softaria.com>
2025-12-02 07:37:05 +0700

    lesson 28 - moved links to sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,20 +1,15 @@
 import cls from "./Navbar.module.scss";
 import { classNames } from "shared/utils/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/appLink/AppLink";
-import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const { t } = useTranslation();
-
   return (
       <div className={classNames(cls.navbar, {}, [className])}>
         <div className={cls.links}>
-          <AppLink theme={AppLinkTheme.SECONDARY} to={`/`}>{t(`Main Page Link`)}</AppLink>
-          <AppLink theme={AppLinkTheme.SECONDARY} to={`/about`}>{t(`About Page Link`)}</AppLink>
+          /
         </div>
       </div>
   );
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -4,12 +4,17 @@ import { useCallback, useState } from "react";
 import { ThemeSwitcher } from "shared/ui/themeSwitcher/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/langSwitcher/LangSwitcher";
 import { AppButton, AppButtonSize, AppButtonTheme } from "shared/ui/appButton/AppButton";
+import { AppLink, AppLinkTheme } from "shared/ui/appLink/AppLink";
+import { useTranslation } from "react-i18next";
+import { RoutePath } from "app/Router/config/routeConfig/routerConfig";
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+  const { t } = useTranslation();
+
   const [collapsed, setCollapsed] = useState(false);
 
   const toggleCollapsed = useCallback(() => { setCollapsed(!collapsed); }, [collapsed]);
@@ -17,6 +22,10 @@ export const Sidebar = ({ className }: SidebarProps) => {
   return (
       <div className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}>
         <AppButton onClick={toggleCollapsed} className={cls.collapseBtn} theme={AppButtonTheme.BACKGROUND_INVERTED} square size={AppButtonSize.L}>{collapsed ? `>` : `<`}</AppButton>
+        <div className={cls.items}>
+          <AppLink theme={AppLinkTheme.SECONDARY} to={RoutePath.main}>{t(`Main Page Link`)}</AppLink>
+          <AppLink theme={AppLinkTheme.SECONDARY} to={RoutePath.about}>{t(`About Page Link`)}</AppLink>
+        </div>
         <div className={cls.switchers}>
           <ThemeSwitcher />
           <LangSwitcher className={cls.lang} short={!collapsed} />
