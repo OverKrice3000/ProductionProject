881949cc958448ee6d37c3033bdc0d19de78d242
igor <i.klimyuk@softaria.com>
2025-12-04 01:14:00 +0700

    lesson 29 - added AppPortal component

---
+++ src/shared/ui/Portal/AppPortal.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+interface AppPortalProps {
+  children: ReactNode;
+  container?: HTMLElement;
+}
+
+export const AppPortal = ({ children, container = document.body }: AppPortalProps) => {
+  return createPortal(children, container);
+};
