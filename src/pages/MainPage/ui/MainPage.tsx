2428158d9308674fd199b7f88827df9360cbc8f0
igor <i.klimyuk@softaria.com>
2025-12-06 08:59:43 +0700

    lesson 30 - removed counter from main page

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from "react-i18next";
 import cls from "./MainPage.module.scss";
-import { Counter } from "entities/counter";
 
 const MainPage = () => {
   const { t } = useTranslation();
@@ -9,7 +8,6 @@ const MainPage = () => {
   return (
         <div className={cls.mainPage}>
           {t(`MainPage`)}
-          <Counter />
         </div>
   );
 };
