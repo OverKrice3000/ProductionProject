11f1faa1f47df828a5066848bd6a01c52209f10e
igor <i.klimyuk@softaria.com>
2025-12-07 07:15:54 +0700

    lesson 30 - removed empty file

--- src/features/authByUsername/ui/loginModal/LoginModal.tsx
+++ src/features/authByUsername/ui/loginModal/LoginModal.tsx
@@ -1,4 +1,3 @@
-import cls from "./LoginModal.module.scss";
 import { classNames } from "shared/utils/classNames";
 import { AppModal } from "shared/ui/appModal/AppModal";
 import { LoginForm } from "features/authByUsername/ui/loginForm/LoginForm";
@@ -11,7 +10,7 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => {
   return (
-    <AppModal className={classNames(cls.Modal, {}, [className])} isOpen={isOpen} onClose={onClose} lazy>
+    <AppModal className={classNames(``, {}, [className])} isOpen={isOpen} onClose={onClose} lazy>
       <LoginForm />
     </AppModal>
   );
