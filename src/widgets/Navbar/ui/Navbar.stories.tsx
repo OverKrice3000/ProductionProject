a44ea59dcbbcda166e149ed02d86d2e487da78d9
igor <i.klimyuk@softaria.com>
2025-12-08 06:27:17 +0700

    lesson 33 - added logged in navbar story; updated screenshots

--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -4,6 +4,7 @@ import type { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Theme } from "shared/utils/theme/ThemeContext";
 import { ThemeDecorator } from "shared/config/storybook/decorator/ThemeDecorator";
 import { Navbar } from "widgets/Navbar";
+import { StateDecorator } from "shared/config/storybook/decorator/StateDecorator";
 
 const meta: ComponentMeta<typeof Navbar> = {
   title: `widgets/Navbar`,
@@ -20,3 +21,14 @@ Light.args = {};
 export const Dark = Template.bind({});
 Dark.args = {};
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {};
+LoggedIn.decorators = [StateDecorator({
+  user: {
+    authData: {
+      id: `1`,
+      username: `username`,
+    },
+  },
+})];
