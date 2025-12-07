dd21db33ec473c57c60f8f2206f6c0bd49729931
igor <i.klimyuk@softaria.com>
2025-12-08 04:18:19 +0700

    lesson 33 - added utility type

--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -1,3 +1,7 @@
 export type ValOf<Object> = Object[keyof Object];
 
 export type Write<OriginalT extends object, ExtendT extends object> = Omit<OriginalT, keyof ExtendT> & ExtendT;
+
+export type Optional<T extends object> = {
+  [K in keyof T]?: T[K];
+};
