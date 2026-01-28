e68ab9a8860be8d45dd3201f141994cf72c13324
igor <borispopygai@gmail.com>
2026-01-28 16:38:26 +0400

    lesson 49: improved use reducer hook

--- src/shared/utils/hooks/useReducer.ts
+++ src/shared/utils/hooks/useReducer.ts
@@ -3,15 +3,19 @@ import { useStore } from "react-redux";
 import type { StoreWithReducerManager } from "app/providers/stateProvider";
 import type { SliceName } from "app/providers/stateProvider/config/stateSchema";
 import type { Reducer } from "@reduxjs/toolkit";
+import { useAppDispatch } from "shared/utils/hooks/useAppDispatch";
 
 export const useReducer = (key: SliceName, reducer: Reducer) => {
   const store = useStore() as StoreWithReducerManager;
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     store.reducerManager.add(key, reducer);
+    dispatch({ type: `@INIT ${key} reducer` });
 
     return () => {
       store.reducerManager.remove(key);
+      dispatch({ type: `@REMOVE ${key} reducer` });
     };
-  }, [key, reducer, store]);
+  }, [dispatch, key, reducer, store]);
 };
