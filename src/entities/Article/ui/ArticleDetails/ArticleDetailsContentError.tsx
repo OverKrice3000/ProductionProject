42a5fd2c91cbf4a84a95184c14d5d208801be375
igor <borispopygai@gmail.com>
2026-02-18 18:09:45 +0400

    lesson 146 - article loading error redesign

--- src/entities/Article/ui/ArticleDetails/ArticleDetailsContentError.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetailsContentError.tsx
@@ -1,9 +1,14 @@
 import { memo } from "react";
 import { useTranslation } from "react-i18next";
 
-import { AppText, TextAlign } from "@/shared/ui/deprecated/AppText";
+import {
+  AppText as AppTextDeprecated,
+  TextAlign,
+} from "@/shared/ui/deprecated/AppText";
 import { AppHStack } from "@/shared/ui/AppStack";
 import { classNames } from "@/shared/utils/classNames";
+import { ToggleFeatures } from "@/shared/utils/features";
+import { AppText } from "@/shared/ui/redesigned/AppText";
 
 interface ArticleDetailsContentErrorProps {
   className?: string;
@@ -15,16 +20,33 @@ export const ArticleDetailsContentError = memo(
     const { t } = useTranslation();
 
     return (
-      <AppHStack
-        justifyContent={`center`}
-        max
-        className={classNames(``, {}, [className])}
-      >
-        <AppText
-          title={t(error ?? `ArticleLoadingError`)}
-          align={TextAlign.CENTER}
-        />
-      </AppHStack>
+      <ToggleFeatures
+        name={`isAppRedesigned`}
+        on={
+          <AppHStack
+            justifyContent={`center`}
+            max
+            className={classNames(``, {}, [className])}
+          >
+            <AppText
+              title={t(error ?? `ArticleLoadingError`)}
+              align={`center`}
+            />
+          </AppHStack>
+        }
+        off={
+          <AppHStack
+            justifyContent={`center`}
+            max
+            className={classNames(``, {}, [className])}
+          >
+            <AppTextDeprecated
+              title={t(error ?? `ArticleLoadingError`)}
+              align={TextAlign.CENTER}
+            />
+          </AppHStack>
+        }
+      />
     );
   },
 );
